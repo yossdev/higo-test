ea99b7e0ba2d7a05f70cddc5685e847fbbd2fb12
yossdev <yosssdev@gmail.com>
2024-10-16 02:29:38 +0700

    fix: 🐛 add suspense boundary

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { Suspense } from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,7 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased container mx-auto`}
       >
-        {children}
+        <Suspense>{children}</Suspense>
       </body>
     </html>
   );
